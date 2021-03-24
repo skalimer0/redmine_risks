6b36ec08bef98d13f57b4a8a54c7084861b976c4
Brice Beaumesnil <brice.beaumesnil@douane.finances.gouv.fr>
2021-03-24 15:07:45 +0100

    minimal matrix

--- app/helpers/risks_helper.rb
+++ app/helpers/risks_helper.rb
@@ -161,7 +161,7 @@ module RisksHelper
     allrisks = []
     (format_risk_levels(Risk::RISK_IMPACT) {|i| format_risk_impact(i)}).each do |i|
       (format_risk_levels(Risk::RISK_PROBABILITY) {|p| format_risk_probability(p)}).each do |p|
-        allrisks[(i[1] << 7) + p[1]] = []
+        allrisks[((i[1] / 25) << 3) + (p[1] / 25)] = []
       end
     end
 
