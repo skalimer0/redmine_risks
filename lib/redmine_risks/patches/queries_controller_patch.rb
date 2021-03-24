84457fdbf34fa7878c5e84c7ab16bf3ee234a07c
Brice Beaumesnil <brice.beaumesnil@douane.finances.gouv.fr>
2021-03-24 14:07:46 +0100

    Like this ?

--- lib/redmine_risks/patches/queries_controller_patch.rb
+++ lib/redmine_risks/patches/queries_controller_patch.rb
@@ -1,18 +1,19 @@
-require_dependency 'journal'
+require_dependency 'queries_controller'
 
 module RedmineRisks
   module Patches
     module QueriesControllerPatch
       def self.included(base) # :nodoc:
         base.send(:include, InstanceMethods)
+        base.class_eval do
+            unloadable # Send unloadable so it will not be unloaded in development
+            def redirect_to_risk_query(options)
+                redirect_to risk_path(@risk)
+            end
         end
       end
-
       module InstanceMethods
         #
-        def redirect_to_risk_query(options)
-            redirect_to risk_path(@risk)
-        end
       end
     end
   end
